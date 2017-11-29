d91c8ff67f14a64fad2f549db506a92cb8383930
LAPTOP-J26LMCR5\ITSA-5 <botcatz314@gmail.com>
2017-11-28 20:18:18 -0700

    Vuforia changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotCatzTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotCatzTeleOp.java
@@ -10,30 +10,30 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
  */
 @TeleOp
 public class BotCatzTeleOp extends LinearOpMode {
-    private DcMotor leftMotor;
-    private DcMotor rightMotor;
-    private DcMotor leftMotor2;
-    private DcMotor rightMotor2;
+   // private DcMotor leftMotor;
+   // private DcMotor rightMotor;
+   // private DcMotor leftMotor2;
+   // private DcMotor rightMotor2;
     private DcMotor slideMotor2;
     private DcMotor slideMotor;
     @Override
     public void runOpMode()
     {
-        leftMotor = hardwareMap.dcMotor.get("leftMotor");
-        rightMotor = hardwareMap.dcMotor.get("rightMotor");
-        rightMotor2 = hardwareMap.dcMotor.get("rightMotor2");
-        leftMotor2 = hardwareMap.dcMotor.get("leftMotor2");
+     //   leftMotor = hardwareMap.dcMotor.get("leftMotor");
+      //  rightMotor = hardwareMap.dcMotor.get("rightMotor");
+      //  rightMotor2 = hardwareMap.dcMotor.get("rightMotor2");
+      //  leftMotor2 = hardwareMap.dcMotor.get("leftMotor2");
         slideMotor = hardwareMap.dcMotor.get("slideMotor");
         slideMotor2 = hardwareMap.dcMotor.get("slideMotor2");
-        leftMotor.setDirection(DcMotor.Direction.REVERSE);
-        leftMotor2.setDirection(DcMotor.Direction.REVERSE);
+      //  leftMotor.setDirection(DcMotor.Direction.REVERSE);
+     //   leftMotor2.setDirection(DcMotor.Direction.REVERSE);
         slideMotor2.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
         while(opModeIsActive()){
-            leftMotor.setPower(-gamepad1.left_stick_y);
-            leftMotor2.setPower(-gamepad1.left_stick_y);
-            rightMotor.setPower(-gamepad1.right_stick_y);
-            rightMotor2.setPower(-gamepad1.right_stick_y);
+       //     leftMotor.setPower(-gamepad1.left_stick_y);
+      //      leftMotor2.setPower(-gamepad1.left_stick_y);
+       //     rightMotor.setPower(-gamepad1.right_stick_y);
+       //     rightMotor2.setPower(-gamepad1.right_stick_y);
             slideMotor2.setPower(-gamepad2.right_stick_y);
             slideMotor.setPower(-gamepad2.right_stick_y);
 
